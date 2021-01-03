b0281cd8119f9bef27766ed2f42e6b2ecfab440b
joel silva <joel.silva.ferr@gmail.com>
2021-01-03 20:56:04 -0300

    snapshot configuration with jest and styled-components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+    <S.Wrapper>
+        <S.Logo
+            src="/img/logo.svg"
+            alt="imagem de um átomo e react avançado escrito ao lado."
+        />
+        <S.Title>React Avançado</S.Title>
+        <S.Description>
+            Typescript, ReactJS, NextJS e Styled Components
+        </S.Description>
+        <S.Illustration
+            src="/img/hero-illustration.svg"
+            alt="Um desenvolver de frente para a tela com código."
+        />
+    </S.Wrapper>
 );
 
 export default Main;
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,15 +3,23 @@ import { render, screen } from '@testing-library/react';
 import Main from '.';
 
 describe('<Main />', () => {
-  it('shout render the heading', () => {
-    const { container } = render(<Main />);
+    it('shout render the heading', () => {
+        const { container } = render(<Main />);
 
-    expect(
-      screen.getByRole('heading', {
-        name: /React avançado/i
-      })
-    ).toBeInTheDocument();
+        expect(
+            screen.getByRole('heading', {
+                name: /React avançado/i
+            })
+        ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
-  });
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should render the colors correctly', () => {
+        const { container } = render(<Main />);
+
+        expect(container.firstChild).toHaveStyle({
+            'background-color': '#06092b'
+        });
+    });
 });
